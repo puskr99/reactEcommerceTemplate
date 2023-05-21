9fce7401561246a3214e583f7487c95ce24d8752
Puskar Adhikari <95041854+puskr99@users.noreply.github.com>
2023-05-21 18:22:52 +0545

    Add files via upload

---
+++ src/components/BaseLayout/components/Footer/index.js
@@ -0,0 +1,12 @@
+//Dependencies
+import React from 'react';
+//Internals
+import './index.css';
+
+const Footer = () => (
+  <div className="footer">
+    <p>© 2023 Copyright Puskar Adhikari</p>
+  </div>
+)
+
+export default Footer;
---
+++ src/components/Data/index.js
@@ -0,0 +1,208 @@
+const products = [
+
+  {
+    id: 18,
+    name: "Black Shirt Men",
+    description: "Black Shirt for Men",
+    price: 990,
+    gender: "men",
+    type: "shirt",
+    img: "https://rukminim1.flixcart.com/image/612/612/xif0q/shirt/t/o/i/s-bcblack108-varani-creation-original-imaghwxzzzkgrxyf.jpeg?q=70",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 16,
+    name: "Man Brown Shirt",
+    description: "Brown Shirt for Men",
+    price: 1300,
+    gender: "men",
+    type: "shirt",
+    img: "https://cdn.shopify.com/s/files/1/0752/6435/products/IMG_0010_51e74518-2782-4b53-8112-a026836a45de.jpg?v=1676529244",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 2,
+    name: "Light Brown Shirt",
+    description: "Light Brown Shirt for Women",
+    price: 600,
+    gender: "women",
+    type: "shirt",
+    img: "https://image.ibb.co/nNmKz5/img2.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+
+  {
+    id: 4,
+    name: "Warm Shirt Women",
+    description: "Woolen Hoodie Women",
+    price: 700,
+    gender: "women",
+    type: "shirt",
+    img: "https://image.ibb.co/dVfORk/img4.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 5,
+    name: "Women Grey Shirt",
+    description: "Light Grey Shirt for Women",
+    price: 700,
+    gender: "women",
+    type: "shirt",
+    img: "https://image.ibb.co/jpMxmk/img5.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 17,
+    name: "Man Black Tie",
+    description: "Black Tie for Men",
+    price: 1200,
+    gender: "men",
+    type: "tie",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTYMqg35z1MZR2OkMTTVPkSryeS-Vz2eP0cdw&usqp=CAU",
+    inCart: false,
+    category: "accessories"
+  },
+
+  {
+    id: 6,
+    name: "Women Red/Brown Shirt",
+    description: "Red/Brown Blouse for Women",
+    price: 1980,
+    gender: "women",
+    type: "blouse",
+    img: "https://image.ibb.co/mJppz5/img6.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 7,
+    name: "Dark Grey Shirt Women",
+    description: "Dark Grey Shirt for Women",
+    price: 600,
+    gender: "women",
+    type: "shirt",
+    img: "https://image.ibb.co/eZiSmk/img7.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 8,
+    name: "White Shirt Women",
+    description: "White Shirt for Women",
+    price: 1450,
+    gender: "women",
+    type: "shirt",
+    img: "https://image.ibb.co/dyCysQ/img8.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 3,
+    name: "Women Grey Shirt",
+    description: "Grey Shirt for Women",
+    price: 500,
+    gender: "women",
+    type: "shirt",
+    img: "https://image.ibb.co/n6iMCQ/img3.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 9,
+    name: "Black Shirt Women",
+    description: "Black Shirt for Women",
+    price: 1660,
+    gender: "women",
+    type: "shirt",
+    img: "https://image.ibb.co/eOYre5/img10.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 10,
+    name: "No Shoulder Hoodie",
+    description: "Hoodie for Women",
+    price: 600,
+    gender: "women",
+    type: "shirt",
+    img: "https://image.ibb.co/f6gcK5/img9.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+
+  {
+    id: 13,
+    name: "Man Black Shirt",
+    description: "Black T-Shirt for Men",
+    price: 1900,
+    gender: "men",
+    type: "shirt",
+    img: "https://i.pinimg.com/originals/96/53/83/965383dfec34d192fd3da3791e122ea6.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 14,
+    name: "Man Grey Tanktop",
+    description: "Grey Tanktop for Men",
+    price: 300,
+    gender: "men",
+    type: "shirt",
+    img: "https://i5.walmartimages.com/asr/8462b9ed-d2c4-457d-9d1c-7f4d539c5e33_1.1e98d316c83337e0cceb61059ef1dfb2.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF",
+    inCart: false,
+    category: "clothes"
+  },
+  {
+    id: 15,
+    name: "Man White Shirt",
+    description: "White Shirt for Men",
+    price: 2000,
+    gender: "men",
+    type: "shirt",
+    img: "https://static-01.daraz.com.np/p/eef298d3e292e4ca6df68cb3b6eb79ce.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+
+
+  {
+    id: 19,
+    name: "4-Pack Man Ties",
+    description: "Ties for Men",
+    price: 350,
+    gender: "men",
+    type: "tie",
+    img: "https://m.media-amazon.com/images/I/81oXSMr91WL._AC_UY1000_.jpg",
+    inCart: false,
+    category: "accessories"
+  },
+  {
+    id: 20,
+    name: "Man Black Tie",
+    description: "Black Tie for Men",
+    price: 1700,
+    gender: "men",
+    type: "tie",
+    img: "https://i.shgcdn.com/7e630a7f-bea0-4345-b85c-cda61c7aefcf/-/format/auto/-/preview/3000x3000/-/quality/lighter/",
+    inCart: false,
+    category: "accessories"
+  },
+  {
+    id: 1,
+    name: "Brown Shirt",
+    description: "Brown T-Shirt for Women",
+    price: 1200,
+    gender: "women",
+    type: "shirt",
+    img: "https://image.ibb.co/kOhL6k/img1.jpg",
+    inCart: false,
+    category: "clothes"
+  },
+];
+
+export default products;
